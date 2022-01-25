17dea579beaaaa20291ea9545e8f1b998d939582
Lorenagrigoraki <84379082+Lorenagrigoraki@users.noreply.github.com>
2022-01-25 12:59:56 +0300

    Update rJS.js

--- js/rJS.js
+++ js/rJS.js
@@ -1,3 +1,4 @@
+alert("Дорбый день! Наведите мышкой на элементы на странице. Если в нажмете на пробел соделжимое страницы удалится!")
 let divElem = document.getElementById('divEl');
 console.log(divEl);
 window.addEventListener('keydown', e => {
